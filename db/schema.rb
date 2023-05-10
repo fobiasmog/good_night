916a090c2c04e86633221e41f6f78be9a83b2dde
fobiasmog <zbrejkin@yandex.ru>
2023-05-10 11:55:23 +0200

    users name not null constraint

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_05_09_210506) do
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
