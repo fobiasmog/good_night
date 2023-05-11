a6c6d41098913eb533f0e7dcac45ae6056af381c
fobiasmog <zbrejkin@yandex.ru>
2023-05-11 22:58:53 +0200

    [rubocop] Style/FrozenStringLiteralComment autocorrect

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class SleepRecord < ApplicationRecord
   self.per_page = 10
 
--- db/migrate/20230511141018_create_user_friends.rb
+++ db/migrate/20230511141018_create_user_friends.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class CreateUserFriends < ActiveRecord::Migration[7.0]
   def change
     create_table :user_friends do |t|
