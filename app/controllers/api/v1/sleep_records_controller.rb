c340d6da5deefe1432945b40c31ad78419de89f9
fobiasmog <zbrejkin@yandex.ru>
2023-05-12 10:53:05 +0200

    sleep_record stop service, updated specs, controllers refactoring

--- app/controllers/api/v1/sleep_records_controller.rb
+++ app/controllers/api/v1/sleep_records_controller.rb
@@ -6,41 +6,25 @@ module Api
       # GET /sleep_records
       # list of already stopped (checked-in/completed) records
       def index
-        @sleep_records = current_user.sleep_records.completed.order(created_at: :desc)
+        sleep_records = current_user.sleep_records.completed.order(created_at: :desc)
 
-        render json: @sleep_records
+        render json: sleep_records
       end
 
       # POST /sleep_records
       # new sleep record check-in
       def create
-        clock_in_sleep_record = current_user.sleep_records.new
+        return head :accepted if current_user.sleep_records.active.present?
 
-        return head :created if clock_in_sleep_record.save
-
-        render json: clock_in_sleep_record.errors, status: :unprocessable_entity
+        head :created if current_user.sleep_records.create!
       end
 
       # PATCH/PUT /sleep_records/1
       # mark current sleep record as checked-in
       def update
-        @sleep_record = SleepRecord.find(params[:id])
+        sleep_record = SleepRecords::Stop.new(params[:id]).call
 
-        return render json: @sleep_record if @sleep_record.update(sleep_record_params)
-
-        render json: @sleep_record.errors, status: :unprocessable_entity
-      end
-
-      private
-
-      def sleep_record_params
-        stopped_at = Time.now
-        duration = (stopped_at - @sleep_record.created_at).seconds.to_i
-
-        {
-          duration: duration,
-          stopped_at: stopped_at
-        }
+        render json: sleep_record
       end
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@
 class ApplicationController < ActionController::API
   before_action :current_user
 
+  rescue_from StandardError do |error|
+    render json: { error: error }, status: :unprocessable_entity
+  end
+
   private
 
   def current_user
